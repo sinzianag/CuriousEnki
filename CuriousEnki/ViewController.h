813099bdcaafd74bfba0f08eab1d626ad00c089b
Dev Account <sinziana.gafitanu@gmail.com>
2014-08-21 22:48:29 +0300

    Initial Commit

---
+++ CuriousEnki/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CuriousEnki
+//
+//  Created by Dev Account on 8/21/14.
+//  Copyright (c) 2014 Sinziana Gafitanu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CuriousEnki/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CuriousEnki
+//
+//  Created by Dev Account on 8/21/14.
+//  Copyright (c) 2014 Sinziana Gafitanu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
